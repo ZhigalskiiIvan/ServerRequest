b65e17bfad3639b36bac55454c9c9fc06e8614cd
ZhigalskiiIvan <671342i@gmail.com>
2023-04-22 18:49:37 +0300

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,69 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.net.Socket
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun serverRequest() {
+    val socket = Socket("npm.mipt.ru", 9048)
+
+    val reader = socket.getInputStream().bufferedReader()
+    val writer = socket.getOutputStream().bufferedWriter()
+
+
+    sendAGreeting(writer)
+
+    val sum = returnRESBytesSum(reader)
+    sendASum(writer, sum)
+
+    print("Answer:${reader.readText()}")
 }
+
+fun sendASum(writer: BufferedWriter, sum: Int) {
+    writer.write("SUM${sum}\n")
+    writer.flush()
+}
+
+fun sendAGreeting(writer: BufferedWriter) {
+    writer.write("HELLO\n")
+    writer.flush()
+}
+
+fun returnRESBytesSum(reader: BufferedReader): Int {
+    val receivedBytes = reader.readText().toByteArray()
+
+    var sizeByteIndex: Int? = null
+
+    for (byteNum in 2..receivedBytes.lastIndex) {
+        if ("${receivedBytes[byteNum - 2].toInt().toChar()}" +
+            "${receivedBytes[byteNum - 1].toInt().toChar()}" +
+            "${receivedBytes[byteNum].toInt().toChar()}" == "RES"
+        ) {
+            sizeByteIndex = byteNum + 1
+            break
+        } else continue
+    }
+    if (sizeByteIndex == null) {
+        println("No message was received including \"RES\".")
+        exitProcess(-1)
+    }
+
+    val size = receivedBytes[sizeByteIndex].toUByte().toInt()
+
+    val returningSum =
+        calculateByteSum(receivedBytes.toList().subList(sizeByteIndex + 1, sizeByteIndex + 1 + size).toByteArray())
+
+    println(returningSum)
+    return returningSum
+}
+
+fun calculateByteSum(byteArray: ByteArray): Int {
+    var sum = 0
+    for (byte in byteArray) sum += byte.toUByte().toInt()
+    return sum
+}
+
+fun main() {
+    serverRequest()
+}
+
+
