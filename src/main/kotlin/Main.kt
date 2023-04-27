f1bf2ae4f428add08ac2cfaeeb1b8a39c6da860e
ZhigalskiiIvan <671342i@gmail.com>
2023-04-27 16:27:32 +0300

    rewrite getting answer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
-import java.io.BufferedReader
-import java.io.BufferedWriter
+import java.io.InputStream
+import java.io.OutputStream
 import java.net.Socket
 import kotlin.system.exitProcess
 
@@ -12,42 +12,45 @@ fun serverRequest() {
 
     val socket = Socket("npm.mipt.ru", 9048)
 
-    val reader = socket.getInputStream().bufferedReader()
-    val writer = socket.getOutputStream().bufferedWriter()
+    val inputStream = socket.getInputStream()
+    val outputStream = socket.getOutputStream()
 
-    sendAGreeting(writer)
+    sendAGreeting(outputStream)
 
-    val sum = returnRESBytesSum(reader)
-    sendASum(writer, sum)
+    val sum = returnRESBytesSum(inputStream)
+    sendASum(outputStream, sum)
 
-    // no answer???
-    print("Answer: ${reader.readText()}")
+    // no answer?
+    while (inputStream.available() > 0) println("Answer: ${getAnswer(inputStream)}")
 
     socket.close()
 }
 
-fun sendASum(writer: BufferedWriter, sum: Int) {
+fun getAnswer(inputStream: InputStream): String = inputStream.bufferedReader().readText()
+
+fun sendASum(outputStream: OutputStream, sum: Int) {
     /**
      * Takes the sum and the buffered writer as input
      * and sends a request to the server.
      */
-    writer.write("SUM${sum}\n")
-    writer.flush()
+    outputStream.write("SUM${sum}\n".toByteArray())
+    outputStream.flush()
+    println("Sum posted: $sum.")
 }
 
-fun sendAGreeting(writer: BufferedWriter) {
+fun sendAGreeting(outputStream: OutputStream) {
     /** Sends HELLO to the server. */
-    writer.write("HELLO\n")
-    writer.flush()
+    outputStream.write("HELLO\n".toByteArray())
+    outputStream.flush()
 }
 
-fun returnRESBytesSum(reader: BufferedReader): Int {
+fun returnRESBytesSum(inputStream: InputStream): Int {
     /**
      * Looks for a phrase in the RES phrase, reads the required number
      * of bytes and then reads the bytes themselves if message was received,
      * call function which calculates its sum and returns it.
      */
-    val receivedBytes = reader.readText().toByteArray()
+    val receivedBytes = inputStream.readBytes()
 
     var sizeByteIndex: Int? = null
 
@@ -67,14 +70,10 @@ fun returnRESBytesSum(reader: BufferedReader): Int {
 
     val size = receivedBytes[sizeByteIndex].toUByte().toInt()
 
-    val returningSum =
-        calculateByteSum(receivedBytes.toList().subList(sizeByteIndex + 1, sizeByteIndex + 1 + size).toByteArray())
-
-    println(returningSum)
-    return returningSum
+    return calculateUByteSum(receivedBytes.toList().subList(sizeByteIndex + 1, sizeByteIndex + 1 + size))
 }
 
-fun calculateByteSum(byteArray: ByteArray): Int {
+fun calculateUByteSum(byteArray: List<Byte>): Int {
     /**
      * Calculates the bytes sum from the byteArray,
      * considering them to be unsigned.
