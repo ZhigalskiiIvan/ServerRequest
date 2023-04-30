401aa499beb249a2e4c805a122680f22391af764
ZhigalskiiIvan <671342i@gmail.com>
2023-04-30 13:54:10 +0300

    rewritten docs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,11 @@ import java.io.OutputStream
 import java.net.Socket
 import kotlin.system.exitProcess
 
-
+/**
+ * Creates a socket, buffered reader and writer
+ * and uses them to call functions of sending and receiving requests.
+ */
 fun serverRequest() {
-    /**
-     * Creates a socket, buffered reader and writer
-     * and uses them to call functions of sending and receiving requests.
-     */
 
     val socket = Socket("npm.mipt.ru", 9048)
 
@@ -28,29 +27,28 @@ fun serverRequest() {
 
 fun getAnswer(inputStream: InputStream): String = inputStream.bufferedReader().readText()
 
+/**
+ * Takes the sum and the buffered writer as input
+ * and sends a request to the server.
+ */
 fun sendASum(outputStream: OutputStream, sum: Int) {
-    /**
-     * Takes the sum and the buffered writer as input
-     * and sends a request to the server.
-     */
     outputStream.write("SUM${sum}\n".toByteArray())
     outputStream.flush()
     println("Sum posted: $sum.")
 }
 
+/** Sends HELLO to the server. */
 fun sendAGreeting(outputStream: OutputStream) {
-    /** Sends HELLO to the server. */
     outputStream.write("HELLO\n".toByteArray())
     outputStream.flush()
 }
 
-fun returnRESBytesSum(inputStream: InputStream): Int {
-    /**
-     * Looks for a phrase in the RES phrase, reads the required number
-     * of bytes and then reads the bytes themselves if message was received,
-     * call function which calculates its sum and returns it.
-     */
-    val receivedBytes = inputStream.readBytes()
+/**
+ * Looks for a phrase in the RES phrase, reads the required number
+ * of bytes and then reads the bytes themselves if message was received,
+ * call function which calculates its sum and returns it.
+ */
+fun returnRESBytesSum(inputStream: InputStream): Int {val receivedBytes = inputStream.readBytes()
 
     var sizeByteIndex: Int? = null
 
@@ -73,11 +71,11 @@ fun returnRESBytesSum(inputStream: InputStream): Int {
     return calculateUByteSum(receivedBytes.toList().subList(sizeByteIndex + 1, sizeByteIndex + 1 + size))
 }
 
+/**
+ * Calculates the bytes sum from the byteArray,
+ * considering them to be unsigned.
+ */
 fun calculateUByteSum(byteArray: List<Byte>): Int {
-    /**
-     * Calculates the bytes sum from the byteArray,
-     * considering them to be unsigned.
-     */
     var sum = 0
     byteArray.forEach { sum += it.toUByte().toInt() }
     return sum
